779ce90759639f70dea2004ae8a44b1a9241e398
Nozren <nikrosktt@outlook.com>
2025-01-26 16:08:24 -0600

    Changes made on 1/26/2025

---
+++ Personal Website/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="top-portion">
+        <h2>Nathan Zeleke</h2>
+        <div className="oval"></div>
+        <h2 class = "subtitle">CS Student @ Northwestern playing Tuba</h2>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
